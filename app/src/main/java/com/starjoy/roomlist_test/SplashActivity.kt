9e7c216e414a37d8762a494e142b9a9cab21a1a9
yeo777 <sayhibb77@gmail.com>
2023-06-15 17:55:19 +0900

    어플의 시작화면을 SplashActivity로 변경했다

---
+++ app/src/main/java/com/starjoy/roomlist_test/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.starjoy.roomlist_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        val goMainHandler = Handler
+
+    }
+}
