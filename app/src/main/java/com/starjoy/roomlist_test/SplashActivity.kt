5b4323d99e5a4c1387197cc00d8d7ec2398157c1
yeo777 <sayhibb77@gmail.com>
2023-06-15 17:59:13 +0900

    핸들러를 이용해서 2.5초 후에 메인화면으로 이동하는 작업 실행

--- app/src/main/java/com/starjoy/roomlist_test/SplashActivity.kt
+++ app/src/main/java/com/starjoy/roomlist_test/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.starjoy.roomlist_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,19 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
 
-        val goMainHandler = Handler
+        val goMainHandler = Handler(Looper.getMainLooper())
+
+        goMainHandler.postDelayed( {
+
+                                   // 2.5초 뒤에 실행할 내용
+                                   // 메인화면으로 이동하는 작업
+                                   // splash 액티비티를 종료
+            val goMainIntent = Intent(this, MainActivity::class.java)
+            startActivity(goMainIntent)
+            finish()
+
+
+        }, 2500 )
 
     }
 }
